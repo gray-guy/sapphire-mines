f32213cd91241f0a20beb872b0bda0a7931267de
gray-guy <mshehroz99@hotmail.com>
2022-04-30 06:42:03 +0300

    fixed gas issue

--- js/otherBackend.js
+++ js/otherBackend.js
@@ -111,10 +111,6 @@ async function handleWalletConnection() {
 async function loadAppData(dataAddress) {
     const web3 = window.web3;
 
-    web3.eth.getGasPrice(function(error, result){
-        console.log("Network gas price: " + result);
-    });
-
     web3.eth.getBalance(dataAddress).then((response) => {
     const bal = parseFloat(web3.utils.fromWei(response)).toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]
     walletBalance = bal;
@@ -167,7 +163,9 @@ async function hireMiners() {
     try {
         await sapphiremineContract.methods.buySapphires(referral_wallet).send({
             from: address,
-            value: amount * 10**18 
+            value: amount * 10**18,
+            maxPriorityFeePerGas: null,
+            maxFeePerGas: null
         }).then((txHash) => {
             loadAppData(address);
             callSnackBar("Processinng...", true);
